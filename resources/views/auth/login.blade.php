3c6608695c0273fc2fdf87266b1140e1c7a13e3f
IKRAM-UL-HAQ-001 <mrikramulhaq0005@gmail.com>
2024-10-28 01:10:31 +0500

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,8 @@
         <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80');">
             <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container my-auto">
-                <div class="row">
+                <h1 class="text-white font-weight-bolder text-center mt-2 mb-5" style="">Exchange Management System</h1>
+                <div class="row mt-3">
                     <div class="col-lg-4 col-md-8 col-12 mx-auto">
                         <div class="card z-index-0 fadeIn3 fadeInBottom">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
@@ -31,7 +32,7 @@
                             <div class="card-body">
                                 <!-- Display Validation Errors -->
                                 @if ($errors->any())
-                                    <div class="alert alert-danger">
+                                    <div class="alert alert-danger text-white">
                                         <ul>
                                             @foreach ($errors->all() as $error)
                                                 <li>{{ $error }}</li>
