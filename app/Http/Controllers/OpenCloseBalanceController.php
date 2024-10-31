3b2f952188ee3296e19d05217fb76ff31ffbe1d1
IKRAM-UL-HAQ-001 <mrikramulhaq0005@gmail.com>
2024-11-01 01:36:45 +0500

    updated

--- app/Http/Controllers/OpenCloseBalanceController.php
+++ app/Http/Controllers/OpenCloseBalanceController.php
@@ -25,11 +25,7 @@ class OpenCloseBalanceController extends Controller
             } else {
                 $exchangeId = Auth::user()->exchange_id;
             }
-            return Excel::download(new OpenCloseBalanceListExport($exchangeId), 'openingClosingBalanceRecord.xlsx')
-            ->withHeaders([
-                'X-Frame-Options' => 'DENY', // Prevents framing
-                'Content-Security-Policy' => "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:;"
-            ]);
+            return Excel::download(new OpenCloseBalanceListExport($exchangeId), 'openingClosingBalanceRecord.xlsx');
         }
     }
     
